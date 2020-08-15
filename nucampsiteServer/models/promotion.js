241357e9be3fc0f307306da00fdf2f5def08dae7
Ben Park <rein21st@gmail.com>
2020-08-15 12:57:59 -0400

    updated promotion for nucampsiteServer

--- nucampsiteServer/models/promotion.js
+++ nucampsiteServer/models/promotion.js
@@ -4,24 +4,7 @@ const Schema = mongoose.Schema;
 require('mongoose-currency').loadType(mongoose);
 const Currency = mongoose.Types.Currency;
 
-const commentSchema = new Schema({
-    rating: {
-        type: Number,
-        min: 1,
-        max: 5,
-        required: true
-    },
-    text: {
-        type: String,
-        required: true
-    },
-    author: {
-        type: String,
-        required: true
-    }
-}, {
-    timestamps: true
-});
+
 
 const promotionSchema = new Schema({
     name: {
@@ -47,7 +30,7 @@ const promotionSchema = new Schema({
         type: Boolean,
         default: false
     },
-    comments: [commentSchema]
+    
 }, {
     timestamps: true
 });
